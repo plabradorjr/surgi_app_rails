6f8374dd17ac495ef377c8a1338d3e010eb35409
github username <github email address>
2020-05-27 22:33:15 -0500

    inventory show all page working

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -2,7 +2,7 @@ class InventoriesController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @inventories = Inventory.all
+        @inventories = Inventory.paginate(page: params[:page], per_page: 5)
     end
 
     def show
