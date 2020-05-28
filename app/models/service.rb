61a043b61622ec015e564bfa75681a1808278f15
github username <github email address>
2020-05-28 03:29:49 -0500

    minimum length removed

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ApplicationRecord
 
-    validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+    validates :name, presence: true, length: { maximum: 25 }
     validates_uniqueness_of :name
     has_many :inventory_services
     has_many :inventories, through: :inventory_services
