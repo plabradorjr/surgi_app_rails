6eb2e9387d84ba943eb5b0fd79d10137938974bc
github username <github email address>
2020-05-28 02:01:01 -0500

    form partial created

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -6,7 +6,44 @@ class InventoriesController < ApplicationController
     end
 
     def show
-        
+        set_inventory
+    end
+
+    def new
+        @inventory = Inventory.new
+    end
+
+    def create
+        @inventory = Inventory.new(inventory_params)
+        @inventory.user = current_user
+        if @inventory.save
+            flash[:notice] = "Inventory was created successfully."
+            redirect_to @inventory
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        set_inventory
+    end
+
+
+    private
+
+    def set_inventory
+        @inventory = Inventory.find(params[:id])
+    end
+
+    def inventory_params
+        params.require(:inventory).permit(:name, :location, :info)
+    end
+
+    def require_same_user
+        if current_user != @inventory.user
+            flash[:alert] = "You can only edit or delete your own inventory"
+            redirect_to @inventory
+        end
     end
 
 end
