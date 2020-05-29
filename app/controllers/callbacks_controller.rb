bbd188a729db68d7542cf012b9c28b7ff02a416f
github username <github email address>
2020-05-29 01:03:19 -0500

    link for twitter working, just waiting for twitter dev approval

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,2 +1,9 @@
 class CallbacksController < ApplicationController
+
+    def twitter
+        @user = User.from_omni(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+
+
 end
