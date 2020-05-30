cc1c279cf5dc17b54716bf4e574db7ed263d54c6
github username <github email address>
2020-05-30 04:29:37 -0500

    configured twiter api keys, routes, schema, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :services
   resources :inventories
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
