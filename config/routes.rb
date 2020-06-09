b779481234d26993fe5340c843d1641025a069c0
Pancho Labrador <plabradorjr@gmail.com>
2020-06-09 13:21:11 -0500

    configuring nested routes with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :services
   resources :inventories
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
